7f653d787a382979b833736578908d73a5a5ed4d
marwaoneis <marwa.oneis22@gmail.com>
2023-12-13 23:02:34 +0200

    Edited profile

--- taxi-app-frontend/src/components/Profile/Profile.jsx
+++ taxi-app-frontend/src/components/Profile/Profile.jsx
@@ -4,21 +4,22 @@ import "./style.css";
 
 const Profile = () => {
   return (
-    <div className="flex profile-container center column">
-      <div className="title-header white-text">
+    <div className="flex profile-container full-width full-height column">
+      <div className="profile-header white-text">
         <h1>My Profile</h1>
       </div>
-      <div className="flex">
-        <div className="profile-pic">
+      <div className="flex profile-bg center column gray-bg ">
+        <div className="profile-pic flex center">
           <img src={profile} alt="profile pic" />
         </div>
         <div className="rating"></div>
-      </div>
-      <div className="user-info flex column gray-bg full-width full-height">
-        <div className="user-name black-text">Name: </div>
-        <div className="email black-text">Email: </div>
-        <div className="phone-number black-text">Phone Number: </div>
-        <div className="date black-text">Date Joined: </div>
+
+        <div className="user-info flex column full-height">
+          <div className="user-name black-text">Name: </div>
+          <div className="email black-text">Email: </div>
+          <div className="phone-number black-text">Phone Number: </div>
+          <div className="date black-text">Date Joined: </div>
+        </div>
       </div>
     </div>
   );
