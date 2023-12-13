7343bd8bf049730a8a3e1086576af7c047d2943f
marwaoneis <marwa.oneis22@gmail.com>
2023-12-13 10:13:39 +0200

    Implemented the landing page

--- taxi-app-frontend/src/components/LandingPage/Footer/Footer.jsx
+++ taxi-app-frontend/src/components/LandingPage/Footer/Footer.jsx
@@ -1,11 +1,112 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="footer-container primary-bg center flex column">
+      <div className="flex column top">
+        <p>
+          <a className="footer-link" href="/">
+            SpeedTrip
+          </a>
+        </p>
+        <p>
+          <a className="footer-link" href="/">
+            Visit help center
+          </a>
+        </p>
+      </div>
+      <div className="footer-wrapper flex">
+        <div className="flex">
+          <ul>
+            <li>
+              <a className="footer-link" href="/">
+                Company
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                About us
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                Our Offerings
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                NewsRoom
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                Blogs
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                Careers
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex">
+          <ul>
+            <li>
+              <a className="footer-link" href="/">
+                Products
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                Ride
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                Drive
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                Eat
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                Business
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                Freight
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex">
+          <ul>
+            <li>
+              <a className="footer-link" href="/">
+                Global Citizenship
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                Safety
+              </a>
+            </li>
+            <li>
+              <a className="footer-link" href="/">
+                Diversity and inclusion
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
