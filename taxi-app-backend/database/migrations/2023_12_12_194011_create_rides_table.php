41535b0040f99066f639559274366b52665aad03
Emad Hmady <emadhmady0@gmail.com>
2023-12-13 03:14:13 +0200

    asa

--- taxi-app-backend/database/migrations/2023_12_12_194011_create_rides_table.php
+++ taxi-app-backend/database/migrations/2023_12_12_194011_create_rides_table.php
@@ -23,6 +23,8 @@ return new class extends Migration
             $table->string('feedback_by_driver')->nullable();
             $table->string('status')->default('pending');
             $table->integer('price')->default(10);
+
+
             $table->foreign('passenger_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('driver_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
